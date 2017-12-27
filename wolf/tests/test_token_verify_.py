da68d79e567ce997e2fc4cd84ed075fe4d18e756
pylover <vahid@carrene.com>
2017-12-27 16:49:49 +0330

    Migrating to new test system

---
+++ wolf/tests/test_token_verify_.py
@@ -0,0 +1,113 @@
+import unittest
+
+from nanohttp import settings
+from restfulpy.orm import DBSession
+
+from wolf.models import Token, Cryptomodule
+from wolf.tests.helpers import roles, As, TimeMonkeyPatch, DocumentaryTestCase
+
+
+class VerifyTokenTestCase(DocumentaryTestCase):
+
+
+    @classmethod
+    def application_factory(cls):
+        app = super().application_factory()
+        settings.merge('''
+            oath:
+              window: 10
+        ''')
+        return app
+
+    @classmethod
+    def mockup(cls):
+        mockup_token1 = Token()
+        mockup_token1.name = 'name1'
+        mockup_token1.phone = 1
+        mockup_token1.expire_date = '2059-12-07T18:14:39'
+        mockup_token1.seed = \
+            b'\xda!\x9e\xb6a\xff\x8a9\xf9\x8b\x06\xab\x0b5\xf8h\xf5j\xaaz\xda!\x9e\xb6a\xff\x8a9\xf9\x8b\x06\xab\x0b5' \
+            b'\xf8h\xf5j\xaaz\xda!\x9e\xb6a\xff\x8a9\xf9\x8b\x06\xab\x0b5\xf8h\xf5j\xaaz\xf5j\xaaz'
+        mockup_token1.is_active = True
+
+        mockup_cryptomodule1 = Cryptomodule()
+        mockup_token1.cryptomodule = mockup_cryptomodule1
+
+        DBSession.add(mockup_token1)
+        DBSession.add(mockup_cryptomodule1)
+        DBSession.commit()
+
+        cls.mockup_token1_id = mockup_token1.id
+
+        cls.fake_time1 = 10001000
+        cls.challenge1 = 'testchallenge-1'
+        cls.valid_otp_token1_time1 = '7110'
+        cls.invalid_otp_token1_time1 = '123456'
+
+        cls.fake_time2 = 199919998
+        cls.challenge2 = 'testchallenge-2'
+        cls.valid_otp_token1_time2 = '1251'
+        cls.invalid_otp_token1_time2 = '123456'
+
+        cls.invalid_fake_time = 123456
+
+    def test_verify_token_otp_time(self):
+        mockup_token_id = self.mockup_token1_id
+
+        with TimeMonkeyPatch(self.fake_time1):
+
+            self.call(
+                'Verifying Time based OTP',
+                'VERIFY',
+                f'/apiv1/tokens/token_id: {mockup_token_id}/codes/code: {self.valid_otp_token1_time1}',
+                role=roles.provider
+            )
+
+            self.call(
+                'Trying to verify an invalid code',
+                'VERIFY',
+                f'/apiv1/tokens/token_id: {mockup_token_id}/codes/code: {self.invalid_otp_token1_time1}',
+                status=400,
+                role=roles.provider
+            )
+
+        # # Test on another time
+        # with TimeMonkeyPatch(self.fake_time2):
+        #
+        #     self.call(
+        #
+        #          ,'VERIFY', f'{self.url}/{mockup_token_id}/codes/{self.valid_otp_token1_time2}',
+        #     )
+        #
+        #     self.request(
+        #         As.provider, 'VERIFY', f'{self.url}/{mockup_token_id}/codes/{self.invalid_otp_token1_time2}',
+        #         expected_status=400
+        #     )
+    #
+    #     # Invalid time
+    #     with TimeMonkeyPatch(self.invalid_fake_time):
+    #         self.request(
+    #             As.provider, 'VERIFY', f'{self.url}/{mockup_token_id}/codes/{self.valid_otp_token1_time2}',
+    #             expected_status=400
+    #         )
+    #
+    # def test_verify_token_expiration(self):
+    #     mockup_token_id = self.mockup_token1_id
+    #
+    #     with TimeMonkeyPatch(self.fake_time1):
+    #
+    #         self.request(
+    #             As.provider, 'VERIFY', f'{self.url}/{mockup_token_id}/codes/{self.valid_otp_token1_time1}',
+    #         )
+    #
+    #     future_time = 99999999999
+    #     with TimeMonkeyPatch(future_time):
+    #
+    #         self.request(
+    #             As.provider, 'VERIFY', f'{self.url}/{mockup_token_id}/codes/{self.valid_otp_token1_time1}',
+    #             expected_status=461,
+    #         )
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
