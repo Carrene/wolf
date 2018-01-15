66be5d97c5f150af36fa7ba30f816d41c7800075
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-01-15 11:57:34 +0330

    Fix a bug in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,13 +10,12 @@ def cross_origin_helper_app(environ, start_response):
     def better_start_response(status, headers):
         headers.append(('Access-Control-Allow-Origin', os.environ.get('WOLF_TRUSTED_HOSTS', '*')))
         headers.append(('Access-Control-Allow-Headers', 'Content-Type, Authorization'))
-        headers.append(
-            ('Access-Control-Expose-Headers', 'X-Pagination-Count, X-Pagination-Skip, X-Pagination-Take, X-Identity')
-        )
-        headers.append(
-            ('Access-Control-Allow-Methods', 'LOGIN, LIST, UNLOCK, DEACTIVATE, ACTIVATE, DELETE, EXTEND, GET')
-        )
+        headers.append(('Access-Control-Expose-Headers', 'X-Pagination-Count, X-Pagination-Skip, X-Pagination-Take, X-Identity'))
+        headers.append(('Access-Control-Allow-Methods', 'LOGIN, LIST, UNLOCK, DEACTIVATE, ACTIVATE, DELETE, EXTEND, GET'))
         headers.append(('Access-Control-Allow-Credentials', 'true'))
         start_response(status, headers)
 
     return wolf(environ, better_start_response)
+
+
+app = cross_origin_helper_app
