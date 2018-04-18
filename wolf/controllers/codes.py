bd32cbc7ac2a5f0616edb1ebc791e9f10caea8b5
pylover <vahid.mardani@gmail.com>
2018-04-19 01:01:13 +0430

    Passing all tests

--- wolf/controllers/codes.py
+++ wolf/controllers/codes.py
@@ -23,28 +23,27 @@ class CodesController(RestController):
     def max_tries(self):
         return settings.token.max_consecutive_tries
 
-    @LazyAttribute
-    def connection(self):
-        from wolf import wolf
-        return wolf.engine.connect()
+#     @LazyAttribute
+#     def connection(self):
+#         from wolf import wolf
+#         return wolf.engine.connect()
 
     @LazyAttribute
     def cryptomodules(self):
         modules = {}
-        for m in self.connection.execute(text(
+        for m in DBSession.execute(text(
             'SELECT id, time_interval, one_time_password_length FROM cryptomodule'
             )):
-            print(m)
             modules[m[0]] = m
         return modules
 
     @action
     @prevent_form
     def verify(self, token_id, code):
-        tokens = self.connection.execute(text(
+        tokens = DBSession.execute(text(
             'SELECT seed, consecutive_tries, expire_date, activated_at, cryptomodule_id '
             'FROM token WHERE id = :token_id'),
-            token_id = token_id
+            dict(token_id=token_id)
         ).fetchall()
         if tokens:
             token = tokens[0]
@@ -69,15 +68,17 @@ class CodesController(RestController):
             self.cryptomodules[token[4]][1],  # Time Interval
         )
         if is_valid:
-            self.connection.execute(
+            DBSession.execute(
                 text('UPDATE token SET consecutive_tries=0 WHERE id = :token_id'),
-                token_id=token_id
+                dict(token_id=token_id)
             )
+            DBSession.commit()
         else:
             # Code is not verified
-            self.connection.execute(text(
+            DBSession.execute(text(
                 f'UPDATE token SET consecutive_tries={token[1]+1} WHERE id=:token_id'),
-                token_id=token_id
+                dict(token_id=token_id)
             )
+            DBSession.commit()
             raise HttpBadRequest('Invalid Code')
 
