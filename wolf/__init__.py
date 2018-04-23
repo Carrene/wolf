c14b5e7ba64b2fa5d6853635c837c8e2f0401276
pylover <vahid.mardani@gmail.com>
2018-04-23 17:24:17 +0430

    Moving the Application definition into a compiled cython file, to protect the configuration encryption secret

--- wolf/__init__.py
+++ wolf/__init__.py
@@ -1,75 +1,8 @@
-from os.path import join, dirname
-
-from nanohttp import settings
-from restfulpy import Application as BaseApplication
-from restfulpy.cryptography import AESCipher
-
-from . import basedata
-from .authentication import Authenticator
-from .cli import PinBlockLauncher
-from .controllers.root import Root
+from .application import Application
 
 
 __version__ = '0.22.0a2'
 
 
-class Application(BaseApplication):
-    __authenticator__ = Authenticator()
-    __configuration_cipher__ = AESCipher(b'ced&#quevbot2(Sc')
-    builtin_configuration = """
-    db:
-      url: postgresql://postgres:postgres@localhost/wolf
-      test_url: postgresql://postgres:postgres@localhost/wolf_test
-      administrative_url: postgresql://postgres:postgres@localhost/postgres
-
-    token:
-      max_consecutive_tries: 5
-
-      seed:
-        max_random_try: 3
-        min_sleep_milliseconds: 10
-        max_sleep_milliseconds: 50
-
-    oath:
-      window: 2
-
-    pinblock:
-      key: 1234567890ABCDEF1234567890ABCDEF
-
-    """
-
-    def __init__(self):
-        super().__init__(
-            'wolf',
-            root=Root(),
-            root_path=join(dirname(__file__), '..'),
-            version=__version__,
-        )
-
-    # noinspection PyArgumentList
-    def insert_basedata(self):
-        basedata.insert()
-
-    # noinspection PyMethodMayBeStatic
-    def register_cli_launchers(self, subparsers):
-        """
-        This is a template method
-        """
-        PinBlockLauncher.register(subparsers)
-
-    def configure(self, files=None, **kwargs):
-        super().configure(**kwargs)
-        files = ([files] if isinstance(files, str) else files) or []
-
-        for filename in files:
-            with open(filename, 'rb') as f:
-                header = f.read(4)
-                if header == b'#enc':
-                    content = configuration_cipher.decrypt(f.read())
-                else:
-                    content = header + f.read()
-                settings.merge(content.decode())
-
-
-wolf = Application()
+wolf = Application(__version__)
 
