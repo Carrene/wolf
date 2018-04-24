d2a5ba7c2b442d8d46deeded36e7e09d3322006e
pylover <vahid.mardani@gmail.com>
2018-04-25 02:41:36 +0430

    Configuring travis to upload wikies on gh-pages branch, #51

--- scripts/travis-upload-wiki.sh
+++ scripts/travis-upload-wiki.sh
@@ -5,17 +5,21 @@ set -e # Exit with nonzero exit code if anything fails
 # Save some useful information
 REPO_URL=`git config remote.origin.url`
 SHA=`git rev-parse --verify HEAD`
-VERSION=`grep -P '^__version__ =' wolf/__init__.py | grep -oP \
-  '([1-9]\d*!)?(0|[1-9]\d*)(\.(0|[1-9]\d*))*((a|b|rc)(0|[1-9]\d*))?(\.post(0|[1-9]\d*))?(\.dev(0|[1-9]\d*))?'`
-VERSION="v${VERSION}"
+if [ -z "$1" ]; then
+  VERSION=`grep -P '^__version__ =' wolf/__init__.py | grep -oP \
+    '([1-9]\d*!)?(0|[1-9]\d*)(\.(0|[1-9]\d*))*((a|b|rc)(0|[1-9]\d*))?(\.post(0|[1-9]\d*))?(\.dev(0|[1-9]\d*))?'`
+  VERSION="v${VERSION}"
+else
+  VERSION=$1
+fi
 TARGET="../project-gh-pages/$VERSION"
 # Clone/checkout the gh-pages branch from Github alongside the master branch working copy
 # directory:
 rm -rf ../project-gh-pages
 git -C .. clone git@github.com:Carrene/wolf.git -b gh-pages project-gh-pages
 GIT="git -C ../project-gh-pages"
-$GIT rm $VERSION/\*.md
 mkdir -p $TARGET
+$GIT rm $VERSION/\*.md
 cp data/documentation/*.md $TARGET
 $GIT add $VERSION/\*.md
 $GIT config user.name "Travis CI"
