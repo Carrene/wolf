94ee780be38210398b19da851906841049a7ff6c
pylover <vahid.mardani@gmail.com>
2018-09-02 02:48:22 +0430

    Oathcy deploy key and travis

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Use this to encrypt files
-# $ tar -cf travis-keys.tar travis-oathcy_rsa* travis-wolf_rsa* travis-wiki_rsa*
+# $ tar -cf travis-keys.tar travis-oathcy_rsa* travis-wiki_rsa*
 # $ travis encrypt-file travis-keys.tar
 # $ mv travis-*_rsa* travis-keys.tar ~/Dropbox/carrene-private/github-keys
 
@@ -11,12 +11,10 @@ openssl aes-256-cbc -K $encrypted_5a97bbf310b0_key -iv $encrypted_5a97bbf310b0_i
 tar -xf travis-keys.tar
 
 chmod 600 travis-oathcy_rsa
-chmod 600 travis-wolf_rsa
 chmod 600 travis-wiki_rsa
 
 eval `ssh-agent -s`
 ssh-add travis-oathcy_rsa
-ssh-add travis-wolf_rsa
 ssh-add travis-wiki_rsa
 
 pip3 install -U pip setuptools wheel cython
