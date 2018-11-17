2b0caf183514ba4f0454aed2a6b80ae768484095
pylover <vahid.mardani@gmail.com>
2018-11-17 17:53:30 +0330

    Migrated to latest libraries

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ with open(join(dirname(__file__), 'wolf', '__init__.py')) as v_file:
 
 
 dependencies = [
-    'restfulpy >= 2.5.1',
-    'oathcy >= 1.3.0',
+    'restfulpy >= 2.6.1',
+    'oathcy >= 1.4.0',
     'pycrypto',
 
     # deployment
