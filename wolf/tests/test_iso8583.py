c766d3cd2f00cb3d74d3388c0256bf5cc3de241b
shayan <shayan>
2019-05-16 11:01:25 +0430

    Return host and port on iso8583 server fixture

--- wolf/tests/test_iso8583.py
+++ wolf/tests/test_iso8583.py
@@ -4,12 +4,6 @@ import pytest
 
 
 def test_iso8583_server(run_iso8583_server):
-    with pytest.raises(NotImplementedError), socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM
-    ) as client_socket:
-
-        print('\033[38;5;206;1msomething')
-        port = run_iso8583_server()
-        client_socket.connect(('localhost', port))
-        client_socket.send('Sample message')
+    host, port = run_iso8583_server()
+    print(host, port)
 
