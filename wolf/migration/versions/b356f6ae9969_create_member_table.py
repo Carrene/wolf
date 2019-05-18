44ebc4ace4ad78d7aa5b1901a54461b21fbd09bd
mehdi <45841074+pynuxer@users.noreply.github.com>
2019-05-18 14:58:18 +0430

    Create alembic migration for create member table (#150)

---
+++ wolf/migration/versions/b356f6ae9969_create_member_table.py
@@ -0,0 +1,52 @@
+"""Create member table
+
+Revision ID: b356f6ae9969
+Revises: ef9f84d306d3
+Create Date: 2019-05-16 11:51:32.184191
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b356f6ae9969'
+down_revision = 'ef9f84d306d3'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'member',
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('modified_at', sa.DateTime(), nullable=True),
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('username', sa.Unicode(length=64), nullable=False),
+        sa.Column('password', sa.Unicode(length=128), nullable=False),
+        sa.Column('bank_id', sa.Integer(), nullable=True),
+        sa.Column('type', sa.Unicode(length=50), nullable=False),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(
+        'ix_member_password',
+        'member',
+        ['password']
+    )
+    op.create_index(
+        'ix_member_username',
+        'member',
+        ['username'],
+        unique=True
+    )
+
+def downgrade():
+    op.drop_index(
+        'ix_member_username',
+        table_name='member'
+    )
+    op.drop_index(
+        'ix_member_password',
+        table_name='member'
+    )
+    op.drop_table('member')
