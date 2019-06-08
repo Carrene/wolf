922bfdb6f2302808fcb98e856a46cf81a1fcf4f3
mohammad <mohammadsheikhian70@gmail.com>
2019-06-08 17:29:46 +0430

    Fixed a bug related to the teardown of mockup server

--- wolf/tests/conftest.py
+++ wolf/tests/conftest.py
@@ -1,16 +1,17 @@
-import time
 import socket
 import threading
+import time
 from os.path import dirname
 
-import pytest
 import pymlconf
+import pytest
 from nanohttp import settings
 from nanohttp.configuration import configure
 from restfulpy.orm import DBSession
+from sqlalchemy.orm.session import close_all_sessions
 
-from wolf.iso8583 import listen
 from wolf.application import Wolf
+from wolf.iso8583 import listen
 
 
 context = dict(
@@ -19,35 +20,38 @@ context = dict(
 )
 
 
+start_event = threading.Event()
+
+
 @pytest.fixture
 def free_port():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.bind((socket.gethostname(), 0))
         return s.getsockname()[1]
+
     finally:
         s.close()
 
 
 @pytest.fixture
-def run_iso8583_server(free_port):
+def iso8583_server(free_port):
     time.sleep(1)
     try:
         settings.merge(Wolf.__configuration__)
+
     except pymlconf.ConfigurationNotInitializedError:
         configure(Wolf.__configuration__, context=context)
 
     thread = threading.Thread(
         target=listen,
-        args=('localhost', free_port),
+        args=('localhost', free_port, start_event),
         daemon=True
     )
-
-    def wrapper(*args, **kw):
-        thread.start()
-        return 'localhost', free_port
-
-    yield wrapper
-    DBSession.close_all()
-    thread.join(1)
+    thread.start()
+    start_event.wait(1)
+    yield 'localhost', free_port
+    engine = DBSession.bind
+    close_all_sessions()
+    engine.dispose()
 
