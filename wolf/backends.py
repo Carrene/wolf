7089bdf308d3e7237df39dc8f47a9459dabf1207
donatello <majid.ghassaei@gmail.com>
2019-07-20 12:21:20 +0430

    Implemented certificate fingerprint for connecting to backend

--- wolf/backends.py
+++ wolf/backends.py
@@ -3,6 +3,9 @@ import urllib
 import binascii
 
 import requests
+from requests import Request, Session
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
 import zeep
 from nanohttp import settings, HTTPKnownStatus
 from restfulpy.logging_ import get_logger
@@ -19,26 +22,62 @@ from .exceptions import DeviceNotFoundError, SSMInternalError, \
 logger = get_logger()
 
 
+class FingerprintAdapter(HTTPAdapter):
+
+    def __init__(self, fingerprint=None, **kwargs):
+        self.fingerprint = str(fingerprint)
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            assert_fingerprint=self.fingerprint
+        )
+
+
 class LionClient:
 
     def __init__(self):
         self.base_url = f'{settings.ssm.url}/apiv1'
         self.token = settings.ssm.token
 
-    def _request(self, key, verb, data):
-        try:
-            response = requests.request(
-                verb,
-                f'{self.base_url}/keys/{key}',
-                data=data,
-                headers={'Authorization': self.token}
+    def send_request(self, *args, **kwargs):
+        session = Session()
+        session.verify = False
+        if settings.ssm.tls.verify:
+            session.mount(
+                f'{self.base_url}/keys',
+                FingerprintAdapter(settings.ssm.tls.fingerprint)
             )
 
+        request = Request(*args, **kwargs)
+        prepped = session.prepare_request(request)
+        response = session.send(prepped)
+        return response
+
+    def encrypt(self, bank_id, phone, data, checksum=4):
+        data = base64.urlsafe_b64encode(data)
+        parameters = dict(
+            data=dict(
+                data=data,
+                checksumLength=checksum
+            ),
+            headers={'Authorization': self.token}
+        )
+
+        try:
+            response = self.send_request(
+                'ENCRYPT',
+                f'{self.base_url}/keys/{bank_id}{phone}',
+                **parameters
+            )
             if response.status_code == 401:
                 raise SSMUnauthorizedError()
 
             if response.status_code == 404:
-                raise DeviceNotFoundError(key)
+                raise DeviceNotFoundError(f'{bank_id}{phone}')
 
             if response.status_code == 502:
                 raise SSMIsNotAvailableError()
@@ -51,16 +90,7 @@ class LionClient:
             logger.exception(ex)
             raise SSMIsNotAvailableError()
         else:
-            return response
-
-    def encrypt(self, bank_id, phone, data, checksum=4):
-        data = base64.urlsafe_b64encode(data)
-        response = self._request(
-            f'{bank_id}{phone}',
-            'ENCRYPT',
-            dict(data=data, checksumLength=checksum)
-        )
-        return base64.urlsafe_b64decode(response.json().encode())
+            return base64.urlsafe_b64decode(response.json().encode())
 
 
 class MaskanClient:
